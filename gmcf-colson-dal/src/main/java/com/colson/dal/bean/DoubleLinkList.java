0247d5db95034e5036a11149bac62935abd3564b
songbowen <670317458@qq.com>
2019-06-20 23:39:58 +0800

    更新代码

--- gmcf-colson-dal/src/main/java/com/colson/dal/bean/DoubleLinkList.java
+++ gmcf-colson-dal/src/main/java/com/colson/dal/bean/DoubleLinkList.java
@@ -1,9 +1,33 @@
 package com.colson.dal.bean;
 
-public class DoubleLinkList {
 
+import java.util.LinkedList;
+
+public class DoubleLinkList<E> {
+
+    Node<E> first;
+
+    Node<E> last;
+
+    int size;
+
+    Node node = new Node();
 
     private static class Node<E>{
 
+        Node<E> prev;
+
+        Node<E> next;
+
+        E data;
+
+        public Node() {
+        }
+
+        private Node(Node<E> prev, E data, Node<E> next) {
+            this.prev = prev;
+            this.next = next;
+            this.data = data;
+        }
     }
 }
