0c177e6133ae3feb278b87c92b4f488b3ed508da
songbowen <670317458@qq.com>
2019-07-22 18:12:19 +0800

    优化

--- gmcf-colson-dal/src/test/java/com/colson/dal/linked/StaticLinkedListTest.java
+++ gmcf-colson-dal/src/test/java/com/colson/dal/linked/StaticLinkedListTest.java
@@ -15,39 +15,39 @@ public class StaticLinkedListTest {
         System.out.println(list);
         list.add(1);
         list.add(2);
-        System.out.println("单链表的结构为："+list);
+        System.out.println("静态链表的结构为："+list);
 
         list.add(3);
         list.add(6);
         list.add(8);
         list.add(7);
         list.add(4);
-        System.out.println("单链表的结构为："+list);
-        System.out.println("单链表的长度为："+list.size());
-        System.out.println("单链表下标为"+index+"的数据为："+list.get(index));
-        System.out.println("单链表中数据为"+data+"的下标为："+list.indexOf(data));
-        System.out.println("单链表的结构为："+list);
-        System.out.println("单链表的结构为："+list);
-        System.out.println("单链表的结构为："+list);
-        System.out.println("单链表的结构为："+list);
+        System.out.println("静态链表的结构为："+list);
+        System.out.println("静态链表的长度为："+list.size());
+        System.out.println("静态链表下标为"+index+"的数据为："+list.get(index));
+        System.out.println("静态链表中数据为"+data+"的下标为："+list.indexOf(data));
+        System.out.println("静态链表的结构为："+list);
+        System.out.println("静态链表的结构为："+list);
+        System.out.println("静态链表的结构为："+list);
+        System.out.println("静态链表的结构为："+list);
 
         System.out.println("----------------分界线------------------");
         list.remove(0);
-        System.out.println("单链表的结构为："+list);
+        System.out.println("静态链表的结构为："+list);
 //        list.remove(list.size();//下标越界
-//        System.out.println("单链表的结构为："+list);
+//        System.out.println("静态链表的结构为："+list);
         list.remove(list.size()-1);
-        System.out.println("单链表的结构为："+list);
+        System.out.println("静态链表的结构为："+list);
         list.remove(list.size()-1);
-        System.out.println("单链表的结构为："+list);
+        System.out.println("静态链表的结构为："+list);
         list.remove(list.size()-1);
-        System.out.println("单链表的结构为："+list);
+        System.out.println("静态链表的结构为："+list);
         list.remove(list.size()-1);
-        System.out.println("单链表的结构为："+list);
+        System.out.println("静态链表的结构为："+list);
         list.remove(list.size()-1);
-        System.out.println("单链表的结构为："+list);
+        System.out.println("静态链表的结构为："+list);
         list.remove(list.size()-1);
-        System.out.println("单链表的结构为："+list);
+        System.out.println("静态链表的结构为："+list);
 
     }
 }
