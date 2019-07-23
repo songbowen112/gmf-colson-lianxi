679b9faee6c1abd9cfeef9822250cb1f5c35778e
songbowen <670317458@qq.com>
2019-07-23 21:56:12 +0800

    二叉树定义

---
+++ gmcf-colson-dal/src/main/java/com/colson/dal/util/BinaryTree.java
@@ -0,0 +1,41 @@
+package com.colson.dal.util;
+
+import com.colson.dal.bean.TreeNode;
+
+import java.util.TreeSet;
+
+/**
+ *
+ * @ClassName:
+ * @Description: 二叉树的定义
+ * @author
+ * @date 2019-7-23 晚上21:32:49
+ */
+public class BinaryTree<E> {
+
+    /**
+     * 查找
+     * @param key
+     * @return
+     */
+    public TreeNode find(E key) {
+        return null;
+    }
+
+    /**
+     * 插入操作
+     * @param value
+     */
+    public void insert(E value) {
+
+    }
+
+    /**
+     * 中序遍历
+     * @param treeNode
+     */
+    public void inOrder(TreeNode treeNode) {
+
+    }
+
+}
