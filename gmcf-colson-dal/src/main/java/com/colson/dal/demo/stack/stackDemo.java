0960fa2dc0f4913931aca1ca62c75e895403ce8a
songbowen <670317458@qq.com>
2019-07-25 18:38:59 +0800

    优化

--- gmcf-colson-dal/src/main/java/com/colson/dal/demo/stack/stackDemo.java
+++ gmcf-colson-dal/src/main/java/com/colson/dal/demo/stack/stackDemo.java
@@ -1,8 +1,16 @@
 package com.colson.dal.demo.stack;
 
+import com.colson.dal.util.SeqStack;
+
 /**
  * 设有两个栈s1，s2都采用顺序栈方式，并共享一个存储区[0,...,maxsize-1]，为了尽量利用空间，减少溢出的可能，
  * 可采用栈顶相向、迎面增长的存储方式。设计出入栈操作算法。
  */
 public class stackDemo {
+
+    public static void main(String[] args) {
+        SeqStack seqStack = new SeqStack();
+    }
+
+
 }
---
+++ gmcf-colson-dal/src/main/java/com/colson/dal/util/TwoWaySeqStack.java
@@ -0,0 +1,14 @@
+package com.colson.dal.util;
+
+import java.io.Serializable;
+
+/**
+ * 双向顺序栈
+ */
+public class TwoWaySeqStack implements Serializable {
+
+    /**
+     * 栈中数据
+     */
+    private Object[] data = new Object[100];
+}
