4447e5fd77f7f0ca6c4451404be43648ff3659ea
songbowen <670317458@qq.com>
2019-08-01 17:19:09 +0800

    zzz优化

--- gmcf-colson-dal/src/main/java/com/colson/dal/demo/stack/stackDemo.java
+++ gmcf-colson-dal/src/main/java/com/colson/dal/demo/stack/stackDemo.java
@@ -10,6 +10,7 @@ public class stackDemo {
 
     public static void main(String[] args) {
         SeqStack seqStack = new SeqStack();
+
     }
 
 
--- gmcf-colson-dal/src/main/java/com/colson/dal/util/TwoWaySeqStack.java
+++ gmcf-colson-dal/src/main/java/com/colson/dal/util/TwoWaySeqStack.java
@@ -1,14 +1,85 @@
 package com.colson.dal.util;
 
 import java.io.Serializable;
+import java.util.Arrays;
 
 /**
  * 双向顺序栈
  */
-public class TwoWaySeqStack implements Serializable {
+public class TwoWaySeqStack<E> implements Serializable {
 
     /**
-     * 栈中数据
+     * 共享存储区
      */
-    private Object[] data = new Object[100];
+    private Object[] datas = new Object[100];
+
+    /**
+     * 左栈顶下标
+     */
+    private int leftIndex;
+
+    /**
+     * 右栈顶下标
+     */
+    private int rightIndex;
+
+    public TwoWaySeqStack() {
+    }
+
+    /**
+     * 入左栈
+     * @param data
+     */
+    public void pushLeft(E data) {
+        if (leftIndex != rightIndex) {
+
+        }
+    }
+
+    /**
+     * 入右栈
+     * @param data
+     */
+    public void pushRight(E data) {
+
+    }
+
+    /**
+     * 出左栈
+     * @return
+     */
+    public E popLeft() {
+        return null;
+    }
+
+    /**
+     * 出右栈
+     * @return
+     */
+    public E popRight() {
+        return null;
+    }
+
+    /**
+     * 获取栈的大小
+     */
+    public int size() {
+        return datas.length;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+
+        return sb.toString().isEmpty()?"[]":"["+sb.toString().substring(0,sb.length()-1)+"]";
+    }
+
+    /**
+     * 下标越界信息
+     * @param index
+     * @return
+     */
+    private String outOfBoundsMsg(int index) {
+        return "Index: "+index+", Size: "+this.datas.length;
+    }
 }
