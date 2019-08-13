fff66734eb9c28dcdef53cf05dbed7b1b2db3712
songbowen <670317458@qq.com>
2019-08-13 18:38:31 +0800

    NoTask:优化递归

--- gmcf-colson-dal/src/main/java/com/colson/dal/demo/stack/StackDemo4.java
+++ gmcf-colson-dal/src/main/java/com/colson/dal/demo/stack/StackDemo4.java
@@ -5,6 +5,15 @@ import com.colson.dal.util.stack.LinkedStack;
 /**
  * 使用递归求解n的阶乘(递归分解过程离不开栈)
  * n! = 1*2*3*4*...*n;
+ * getF(3)=getF(2)*3=getF(1)*2*3=getF(0)*1*2*3
+ * 递归分解3的阶乘:
+ * 1.执行getF(2)*3时将getF(2)入栈,栈中数据:[getF(2)]
+ * 2.执行getF(1)*2时将getF(1)入栈,栈中数据:[getF(1),getF(2)]
+ * 3.执行getF(0)*1时将getF(0)入栈,栈中数据:[getF(0),getF(1),getF(2)]
+ * 4.当getF(0)返回1时,getF(0)出栈并赋值为1
+ * 5.计算getF(0)*1得1,getF(1)出栈并赋值为1
+ * 6.计算getF(1)*2得2,getF(2)出栈并赋值为2
+ * 7.计算getF(2)*3得6,getF(3)得到最终结果为6
  */
 public class StackDemo4 {
 
@@ -31,9 +40,10 @@ public class StackDemo4 {
             n = n-1;
         }
         Integer result = 1;
-        for (int i=0;i<stack.size();i++) {
+        int size = stack.size();
+        for (int i=0;i<size;i++) {
             Integer pop = stack.pop();
-            result += result*pop;
+            result *= pop;
         }
         return result;
     }
