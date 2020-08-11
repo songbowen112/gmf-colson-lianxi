b53d616115885f33f439d11a45bdcca104994266
songbowen <670317458@qq.com>
2020-08-11 11:18:30 +0800

    NoTask:重要日期test

---
+++ gmcf-colson-dal/src/test/java/com/colson/dal/ImportanceDateTest.java
@@ -0,0 +1,28 @@
+package com.colson.dal;
+
+import com.colson.dal.util.DateUtils;
+
+import java.text.ParseException;
+
+public class ImportanceDateTest {
+
+    private static String date = "2018-6-27 00:00:00";
+
+    private static String date2 = "2020-6-27 00:00:00";
+
+    private static String date3 = "2020-1-1 00:00:00";
+
+    public static void main(String[] args) {
+        try {
+            System.out.println(DateUtils.datesub(date,DateUtils.getStringDate()));
+
+            System.out.println(DateUtils.datesub(date2,DateUtils.getStringDate()));
+
+            System.out.println(DateUtils.datesub(date3,DateUtils.getStringDate()));
+
+
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
+}
