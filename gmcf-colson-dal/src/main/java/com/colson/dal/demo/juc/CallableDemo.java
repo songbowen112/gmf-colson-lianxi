8fdb20a845cf8beb8ee9ac03c2ea0f2361b239f3
songbowen <670317458@qq.com>
2020-09-18 18:44:46 +0800

    多线程

---
+++ gmcf-colson-dal/src/main/java/com/colson/dal/demo/juc/CallableDemo.java
@@ -0,0 +1,9 @@
+package com.colson.dal.demo.juc;
+
+public class CallableDemo {
+
+    public static void main(String[] args) {
+
+    }
+
+}
--- gmcf-colson-dal/src/main/java/com/colson/dal/demo/juc/ConditionDemo.java
+++ gmcf-colson-dal/src/main/java/com/colson/dal/demo/juc/ConditionDemo.java
@@ -1,5 +1,7 @@
 package com.colson.dal.demo.juc;
 
+import org.apache.commons.lang3.StringUtils;
+
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
@@ -8,61 +10,74 @@ class ShareData {
 
     private Integer flag = 0;//标志位
 
-    private Lock lock = new ReentrantLock();
-    private Condition c1 = lock.newCondition();
-    private Condition c2 = lock.newCondition();
-    private Condition c3 = lock.newCondition();
+    private Lock lock = new ReentrantLock();//锁
+    private Condition c1 = lock.newCondition();//钥匙1
+    private Condition c2 = lock.newCondition();//钥匙2
+    private Condition c3 = lock.newCondition();//钥匙3
+    private Condition current = null;//当前condition
+    private Condition next = null;//下个condition
 
-    public void printABC() throws Exception {
+    public void print() throws Exception {
+        String threadName = Thread.currentThread().getName();
 
+        if (StringUtils.isNotBlank(threadName)) {
+            if ("A".equals(threadName)) {
+                current = c1;
+                next = c2;
+                flag = 5;
+            } else if ("B".equals(threadName)) {
+                current = c2;
+                next = c3;
+                flag = 10;
+            } else if ("C".equals(threadName)) {
+                current = c3;
+                next = c1;
+                flag = 15;
+            }
+        }
 
+        lock.lock();
+        while (flag != 5) {
+            current.await();
+        }
         try {
-            lock.lock();
-            while (flag != 0) {
-                c1.await();
+            for (int i = 0; i < flag; i++) {
+                System.out.println(threadName + "     " + (i + 1));
             }
-            if (flag == 0) {
-                for (int i = 0; i < 5; i++) {
-                    System.out.println(Thread.currentThread().getName() + "     " + (i + 1));
-                }
-                flag = 1;
-                c2.signal();
-            }
+            flag = 10;
+            next.signal();
         } finally {
             lock.unlock();
         }
 
-        try {
-            lock.lock();
-            while (flag != 1) {
-                c2.await();
-            }
-            if (flag == 1) {
-                for (int i = 0; i < 10; i++) {
-                    System.out.println(Thread.currentThread().getName() + "     " + (i + 1));
-                }
-                flag = 2;
-                c3.signal();
-            }
-        } finally {
-            lock.unlock();
-        }
 
-        try {
-            while (flag != 2) {
-                c3.await();
-            }
-            if (flag == 2) {
-                for (int i = 0; i < 15; i++) {
-                    System.out.println(Thread.currentThread().getName() + "     " + (i + 1));
-                }
-                flag = 0;
-                c1.signal();
-            }
-        } finally {
-            lock.unlock();
-        }
+//        try {
+//            while (flag != 0) {
+//                for (int i = 0; i < 5; i++) {
+//                    System.out.println(threadName + "     " + (i + 1));
+//                }
+//                flag = 1;
+//                c2.signal();
+//            }
+//            while (flag != 1) {
+//                for (int i = 0; i < 10; i++) {
+//                    System.out.println(threadName + "     " + (i + 1));
+//                }
+//                flag = 2;
+//                c3.signal();
+//            }
+//            while (flag != 2) {
+//                for (int i = 0; i < 15; i++) {
+//                    System.out.println(threadName + "     " + (i + 1));
+//                }
+//                flag = 0;
+//                c1.signal();
+//            }
+//        } finally {
+//            lock.unlock();
+//        }
     }
+}
 
 //    public void printA() throws Exception {
 //        lock.lock();
@@ -73,7 +88,7 @@ class ShareData {
 //            }
 //            //2 干活
 //            for (int i = 0; i < 5; i++) {
-//                System.out.println(Thread.currentThread().getName() + "     " + (i + 1));
+//                System.out.println(threadName + "     " + (i + 1));
 //            }
 //            //3 通知
 //            //注意要先改标志位
@@ -90,7 +105,7 @@ class ShareData {
 //                c2.await();
 //            }
 //            for (int i = 0; i < 10; i++) {
-//                System.out.println(Thread.currentThread().getName() + "     " + (i + 1));
+//                System.out.println(threadName + "     " + (i + 1));
 //            }
 //            flag = 2;
 //            c3.signal();
@@ -105,7 +120,7 @@ class ShareData {
 //                c3.await();
 //            }
 //            for (int i = 0; i < 15; i++) {
-//                System.out.println(Thread.currentThread().getName() + "     " + (i + 1));
+//                System.out.println(threadName + "     " + (i + 1));
 //            }
 //            flag = 0;
 //            c1.signal();
@@ -113,7 +128,6 @@ class ShareData {
 //            lock.unlock();
 //        }
 //    }
-}
 
 /**
  * 备注：多线程之间按顺序调用，实现A->B->C
@@ -124,7 +138,7 @@ class ShareData {
  * AA打印5次，BB打印10次，CC打印15次
  * 来10轮
  */
-public class ConditionDemo06 {
+public class ConditionDemo {
 
     public static void main(String[] args) {
         ShareData shareData = new ShareData();
@@ -132,28 +146,31 @@ public class ConditionDemo06 {
         new Thread(() -> {
             try {
                 for (int i = 0; i < 10; i++) {
-                    shareData.printABC();
+                    shareData.print();
                 }
             } catch (Exception e) {
+                System.out.println(e.getMessage());
                 e.printStackTrace();
             }
         }, "A").start();
         new Thread(() -> {
             try {
                 for (int i = 0; i < 10; i++) {
-                    shareData.printABC();
+                    shareData.print();
                 }
             } catch (Exception e) {
+                System.out.println(e.getMessage());
                 e.printStackTrace();
             }
         }, "B").start();
         new Thread(() -> {
             try {
                 for (int i = 0; i < 10; i++) {
-                    shareData.printABC();
+                    shareData.print();
                     System.out.println("--------------------------------");
                 }
             } catch (Exception e) {
+                System.out.println(e.getMessage());
                 e.printStackTrace();
             }
         }, "C").start();
