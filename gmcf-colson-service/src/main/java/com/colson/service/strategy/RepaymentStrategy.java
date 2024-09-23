12dbe62d17b157683d26e6749f2b7a648275bcc1
songbowen <songbowen@ronjusk.com>
2024-09-23 10:15:47 +0800

    加注释

--- gmcf-colson-service/src/main/java/com/colson/service/strategy/RepaymentStrategy.java
+++ gmcf-colson-service/src/main/java/com/colson/service/strategy/RepaymentStrategy.java
@@ -2,6 +2,9 @@ package com.colson.service.strategy;
 
 import com.colson.service.context.RepaymentContext;
 
+/**
+ * 还款策略接口
+ */
 public interface RepaymentStrategy {
     void process(RepaymentContext repaymentContext);
 }
